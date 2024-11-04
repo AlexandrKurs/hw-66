28c3398a548c171373133174a809c92903fcd3ea
Alexandr Kurganov <alexandr.kurs.it@gmail.com>
2024-11-04 12:45:20 +0600

    Add App.css, create structure App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 const App = () => {
-
   return (
-    <>
-      Hello!
-    </>
-  )
-
+    <div className="container mt-5">
+      <div className="text-center mb-4 custom-header">
+        <h1 className="title">Calorie Tracker</h1>
+      </div>
+      <div className="toolbar d-flex justify-content-between align-items-center mb-4">
+        <p>Total calories: 900 kcal</p>
+        <button className="btn btn-primary">Add new meal</button>
+      </div>
+      <div className="meal-list">
+        <ul className="list-group">
+          <li className="list-group-item">
+            <div className="d-flex justify-content-between">
+              <div>
+                <strong>Breakfast</strong>: Pancakes, Bacon, Fruit
+              </div>
+              <span>300 kcal</span>
+            </div>
+          </li>
+          <li className="list-group-item">
+            <div className="d-flex justify-content-between">
+              <div>
+                <strong>Lunch</strong>: Salad, Chicken
+              </div>
+              <span>400 kcal</span>
+            </div>
+          </li>
+          <li className="list-group-item">
+            <div className="d-flex justify-content-between">
+              <div>
+                <strong>Dinner</strong>: Pasta, Vegetables
+              </div>
+              <span>200 kcal</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
-export default App; 
+
+export default App;
